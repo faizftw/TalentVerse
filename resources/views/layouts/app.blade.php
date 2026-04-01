7676ac4c59cbaf016db9645567642b364e110975
faizftw <faizfatwa030206@gmail.com>
2026-04-01 09:35:21 +0800

    feat: change favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
+    {{-- Favicon --}}
+    <link rel="icon" type="image/png" href="{{ asset('images/tvlogo.png') }}">
+
+
     {{-- Vite Assets --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
